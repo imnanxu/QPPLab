37c33700b3915ea6b31083400340fae3379ac745
Nan Xu <68754220+imnanxu@users.noreply.github.com>
2023-03-08 14:45:38 -0500

    Update PLextension.m

--- QPPfv0922/PLextension.m
+++ QPPfv0922/PLextension.m
@@ -1,4 +1,5 @@
 function [PLh, PLc, PLe]=PLextension(PL)
+% developed by Nan Xu on June 23, 2022.
 PLh=round(PL/2)+[0 -rem(PL,2)]; % pad length for temporally extending a QPP
 PLc=round(PL/2)+(1:PL); % range of interest in an extended QPP, matches PLh
 PLe=PL+sum(PLh); % length of an extended QPP (derivable but saved/read)
