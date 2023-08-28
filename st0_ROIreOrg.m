c79775043184b42179ea590059500df2c1af3ac5
Nan Xu <68754220+imnanxu@users.noreply.github.com>
2023-08-28 17:02:01 -0400

    Update st0_ROIreOrg.m

--- st0_ROIreOrg.m
+++ st0_ROIreOrg.m
@@ -15,5 +15,7 @@ AtlasTable.Label=eval(['AtlasTable.' Label]); AtlasTable.System=eval(['AtlasTabl
 AtlasTable = sortrows(AtlasTable,'Label','ascend');
 [NetLB, net_index, ROI2Net]=unique(AtlasTable.System,'stable'); 
 [nsbj, nscn]=size(D0); [nroi,ntimepoint]=size(D0{1,1});
-if ~isfield(D, 'MotionInf'),MotionInf=repmat({[1:ntimepoint]},nsbj,nscn); end
+if ~isfield(D, 'MotionInf')
+    for i=1:nsbj, for j=1:nscn,[~,ntimepoint]=size(D0{i,j});  MotionInf{i,j}=[1:ntimepoint]; end; end
+end
 save(dataOUT,'D0','MotionInf','ROI2Net','NetLB');
